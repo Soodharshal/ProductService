65f780cf0bfbaadf86e2503c832766e2a8010294
harshal sood <soodharshal@gmail.com>
2023-09-02 17:40:15 +0530

    Product service setup

---
+++ src/test/java/dev/harshal/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.harshal.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
